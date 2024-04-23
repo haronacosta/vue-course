b580d61390c68785a8d8c7c2c8cca2e554d04f39
Haron Acosta <haron.acosta14@gmail.com>
2024-04-23 16:06:14 -0400

    Seccion 03

--- 02-fundamentos-vue/src/App.vue
+++ 02-fundamentos-vue/src/App.vue
@@ -1,9 +1,13 @@
 <script setup>
-import CounterComponent from './components/CounterComponent.vue'
+// import CounterComponent from './components/CounterComponent.vue'
+
+import IndecisionComponent from './components/IndecisionComponent.vue'
 </script>
 
 <template>
-  <CounterComponent title="Entraron" :start="5" />
+  <!-- <CounterComponent title="Entraron" :start="11" /> -->
+
+  <IndecisionComponent />
 </template>
 
 <style scoped></style>
